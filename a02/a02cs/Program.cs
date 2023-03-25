a7cf8b052d4b3a2daa86d15f5f8ae3535c1095a1
httptheus <httptheus404@gmail.com>
2023-03-25 17:53:20 -0300

    25/03/2023

---
+++ a02/a02cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace a02;
+class Program
+{
+    static void Main (string[] args)
+    {
+        Console.WriteLine(retornarMultiplicacao(6));
+    }
+
+    /*O método retornarMultiplicacao() não seria possível de ser utilizado se não fosse um membro estático; elementos estáticos podem ser chamados independentemente de uma classe.*/
+    public static string retornarMultiplicacao(int num){
+        string resultado = "";
+        
+        for (int i = 0; i <= 10; i++){
+            resultado += $"{num} x {i} = {num * i}\n";
+        }
+
+        return resultado;
+    }
+}
