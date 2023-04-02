6939f88e957976bc2bf3c08b15659ac3468105ba
kusopunk <kusopunkk@gmail.com>
2023-04-02 10:45:46 -0300

    02/04/2023

---
+++ a04/a04cs-1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace a04cs1;
+class Program
+{
+    /*Escopo: o método a seguir não pode ser acessado dentro do método Main por questões de: 
+    função não foi declarada dentro do escopo onde vai ser chamada.
+    
+    Há duas soluções possíveis: ou a estrutura do método a seguir é transformado em um método estático, ou
+    ele é recortado para dentro de Main (sem o modificador 'public'))
+    
+    A questão do escopo ainda pode ser observada quando é impossível chamar a variável 'total', 
+    já que ela só existe dentro do escopo da função em que foi declarada.*/
+
+    /*'params' passado dentro do critério de argumentos de método pode receber mais de um valor em uma chamada de método do tipo especificado.*/
+    int somarValores(params int[] valores){
+        int total = 0;
+
+        for (int i = 0; i < valores.Length; i++){
+            total += valores[i];
+        }
+
+        return total;
+    }
+
+    static void Main(string[] args){
+        Console.WriteLine(somarValores(1, 2, 3, 4));
+    }
+}
---
+++ a04/a04cs/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace a04cs;
+class Program
+{
+    /*Criando uma classe Pessoa.*/
+    class Pessoa{
+        private string nome;
+        private int idade;
+
+        public void setNome(string nome){
+            this.nome = nome;
+        }
+
+        public string getNome(){
+            return this.nome;
+        }
+
+        public void setIdade(int idade){
+            this.idade = idade;
+        }
+
+        public int getIdade(){
+            return this.idade;
+        }
+
+        public void verDados(){
+            Console.WriteLine("Nome: {0}\nIdade: {1}", getNome(), getIdade());
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        /*Instanciando um novo objeto de 'Pessoa'.*/
+        Pessoa p1 = new Pessoa();
+        p1.setNome("Matheus");
+        p1.setIdade(19);
+
+        p1.verDados();
+    }
+}
