86712f574b32d1aee344fd6900a69e6d2573dab9
kusopunk <kusopunkk@gmail.com>
2023-04-29 11:17:40 -0300

    29/04/2023

---
+++ a07/a07cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace a07cs;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        //O valor é definido no momento da declaração de uma variável constante.
+        const uint idade = 21;
+
+        //Constantes não podem receber qualquer outro valor depois de serem atribuídas um valor.
+        idade = 32;
+
+        Console.WriteLine(idade);
+    }
+}
