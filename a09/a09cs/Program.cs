4337aab061c3e1ea02157d0dd013d25da0b71a14
kusopunk <kusopunkk@gmail.com>
2023-04-30 15:06:27 -0300

    30/04/2023

---
+++ a09/a09cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace a09cs;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        int valor = 25;
+
+        //Desloca para a esquerda. Multiplica por dois uma vez.
+        Console.WriteLine(valor << 1); //50
+
+        //Desloca para a direita. Divide por dois uma vez.
+        Console.WriteLine(valor >> 1); //12
+    }
+}
---
+++ a09/a09cs2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Binários em C# - atribuímos um valor binário iniciando com o prefixo '0b' seguido
+//da representação binária de um valor. O valor à ser atribuído é o valor decimal inteiro
+//desse valor.
+
+//Atribuindo o valor 25 em binário.
+var bin25 = 0b11001;
+
+//Atribuindo o valor 50 em binário.
+var bin50 = 0b110010;
+
+//Atribuindo o valor 100 em binário.
+var bin100 = 0b1100100;
+
+Console.WriteLine(bin25);
+Console.WriteLine(bin50);
+Console.WriteLine(bin100);
