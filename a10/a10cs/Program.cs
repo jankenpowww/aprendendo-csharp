b77faf76a375d8eaeefae6238d084e495607b122
kusopunk <kusopunkk@gmail.com>
2023-04-30 16:40:30 -0300

    30/04/2023

---
+++ a10/a10cs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace a10cs;
+
+class Program
+{
+    enum diasDaSemana {Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado}
+
+    static void Main (string[] args)
+    {
+        diasDaSemana dia = diasDaSemana.Segunda;
+        
+        // Convertendo o valor explicitamente para o tipo inteiro via typecasting.
+        int numDia = (int) dia;
+
+        Console.WriteLine($"'{dia}' corresponde ao índice {numDia} dentro do enum.");
+    }
+}
