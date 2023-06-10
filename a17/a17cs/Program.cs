90f75b55ccdc1686af0a2b11ccbe28abe5f107af
jankenpowww <kusopunkk@gmail.com>
2023-06-10 16:40:31 -0300

    10/06/2023

---
+++ a17/a17cs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace a17cs;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        //Arrays unidimensionais no C# - armazenam uma coleção de valores de tamanho dinâmico
+        //e de tipo definido.
+        int[] arrayNumeros = {143, 22, 354, 432, 43};
+
+        //Length - propriedade exclusiva de arrays (e strings) que retornam
+        //o comprimento de um valor. No caso das arrays, o número de
+        //elementos.
+        Console.WriteLine("A array possui " + arrayNumeros.Length + " elementos.");
+
+        //Exibindo apenas um valor de um índice existente.
+        Console.WriteLine("O valor presente no índice 3 é {0}.", arrayNumeros[3]);
+
+        //Substituíndo um valor do índice.
+        arrayNumeros[1] = 7;
+
+        //Testando comprimentos de arrays.
+        for (int i = 0; i <= arrayNumeros.Length; i++)
+        {
+            arrayNumeros[i] = arrayNumeros[i] * 2;
+        }
+
+        //Exibindo os valores no console.
+        foreach(int num in arrayNumeros){
+            Console.Write(num + " ");
+        }
+    }
+}
---
+++ a17/a17cs2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace a17cs2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Array de tamanho fixo. Literalmente cria-se uma instÇancia de array de inteiro onde
+        //é informado um tamanho fixo, neste caso, um array de inteiro de 3 elementos.
+        int[] numeros = new int[3];
+
+        numeros[0] = 2;
+        numeros[1] = 43;
+        numeros[2] = 543;
+
+        //Sendo um array fixo de comprimento 3, há apenas três índices de espaço disponíveis,
+        //indo de 0 à 2. Um novo índice de 3 em diante é impossível.
+        numeros[3] = 74;
+
+        Console.WriteLine(numeros.Length);
+    }
+}
